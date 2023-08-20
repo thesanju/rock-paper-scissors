27f44d2c26f5f7c45c2eeb6e41f53145bf76c9a2
thesanju <sanjayjr1official@gmail.com>
2023-08-20 12:57:49 +0530

    added a function

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 function rock() {
-    console.log(Math.random());
+    const randomNumber = Math.random();
+
+    let computerMove = '';
+
+    if (randomNumber >= 0 && randomNumber < 1 / 3) {
+      computerMove = 'rock';
+    } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+      computerMove = 'paper';
+    } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+      computerMove = 'scissors';
+    }
+  console.log(computerMove);
+
+  let result = '';
+
+  if (computerMove === 'rock') {
+    result = 'Tie';
+  } else if (computerMove === 'paper') {
+    result = 'Loose';
+  } else if ( computerMove === 'scissors') {
+    result = 'Win';
+  }
+  alert(`You picked rock computer picked ${computerMove} and you ${result}`)
 }
+
+
+
